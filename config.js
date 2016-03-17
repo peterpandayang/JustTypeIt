244204fcff11b30b67e57f61aa83882aba56b154
peterpandayang <bingkunyangfightforoffer2016@gmail.com>
2016-03-17 12:23:56 -0700

    no message

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	"database": "mongodb://root:123456@ds019078.mlab.com:19078/userstorypeter",
+	"port": process.env.port || 3000,
+	"secretKey": "YourSecrectKey"
+	
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config');	//in the same directory
+var mongoose = require('mongoose');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+
+
+mongoose.connect(config.database, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Connected to the database ');
+	}
+});
+
+app.use(bodyParser.urlencoded({ extended : true })); 	// extended == false only support string
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+
+var api = require('./app/routes/api')(app, express, io);
+app.use('/api', api);
+
+
+app.get('*', function(req, res) {
+	res.sendFile(__dirname + '/public/app/views/index.html');
+})
+
+
+http.listen(config.port, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Listening on port 3000");
+	}
+});
+
+
+
